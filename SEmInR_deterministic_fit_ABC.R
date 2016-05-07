c67108c95c6a33a2b2b548615ac70ed2a227985c
davidchampredon <david.champredon@gmail.com>
2016-05-07 14:55:04 -0400

    add ABC fit

---
+++ SEmInR_deterministic_fit_ABC.R
@@ -0,0 +1,89 @@
+library(snowfall)
+# source("SEmInR_deterministic.R")
+
+
+sum.stat <- function(target, x){
+	n <- length(target)
+	
+	# Summary stat #1
+	s1 <- sqrt(sum(  (target-x[1:n])^2) )
+	
+	# Summary stat #2
+	t.obs <- 1:n
+	d.target <- data.frame(t.obs,logtarget=log(1+target))
+	lm.target <- lm(data = d.target, 
+					formula = logtarget ~ poly(t.obs,degree = 3, raw=TRUE))
+	lm.target.coef <-  lm.target$coefficients
+	ftarget <- lm.target.coef[1] + lm.target.coef[2]*t.obs + lm.target.coef[3]*t.obs^2 + lm.target.coef[4]*t.obs^3
+	
+	d.x <- data.frame(t.obs,logx=log(1+x))
+	lm.x <- lm(data = d.x, 
+			   formula = logx ~ poly(t.obs,degree = 3, raw=TRUE))
+	lm.x.coef <-  lm.x$coefficients
+	
+	fx <- lm.x.coef[1] + lm.x.coef[2]*t.obs + lm.x.coef[3]*t.obs^2 + lm.x.coef[4]*t.obs^3
+	s2 <- sqrt(sum( (fx-ftarget)^2 ))
+	
+	if(F){ # DEBUG
+		plot(t.obs,log(1+target), main=s2,typ='o',ylim=range(log(1+target),fx))
+		lines(t.obs,log(1+x),col='red',typ='o',pch=15)
+		lines(t.obs,ftarget,col='blue',typ='l',pch=15)
+		lines(t.obs,fx,col='red',typ='l',pch=16)
+	}
+	return(s2)
+}
+
+
+sample.priors <- function(priors, n){
+	
+	# number of parameters that will be sampled:
+	n.priors <- length(priors)
+	
+	# matrix that will hold all prior samples
+	M <- matrix(nrow = n, ncol = n.priors)
+	for(j in 1:n.priors){
+		z <- priors[[j]]
+		M[,j] <- do.call(what = paste0("r",z[[1]]) , args = c(n, z[[2]]))
+	}
+	colnames(M) <- names(priors)
+	return(M)
+}
+
+
+fct.wrap <- function(i, t.obs, M, prm.fxd, nABC){
+	sim <- simul.SEmInR.det(prm.to.fit = M[i,], prm.fxd)
+	df  <- sim$ts
+	inc <- df$inc
+	tt  <- df$time
+	
+	dt  <- tt[2]-tt[1]
+	idx <- vector()
+	for(k in 1:length(t.obs)) { idx[k] <- which(abs(tt-t.obs[k]) <= dt/1.99)[1] }
+	t <- tt[idx]
+	
+	return(sum.stat(target = inc.obs, x = inc[idx]))
+}
+
+fit.ABC.SEmInR <- function(t.obs, inc.obs, prm.fxd, priors.prm.to.fit, 
+						   nABC, post.prop){
+	
+	M <- sample.priors(priors.prm.to.fit, nABC)
+	
+	# Parallel execution:
+	sfInit(parallel = TRUE, cpu = parallel::detectCores())
+	sfLibrary(deSolve)
+	sfExportAll()
+	ss <- sfSapply(x = 1:nABC, fun = fct.wrap, 
+				   t.obs=t.obs, 
+				   M=M, prm.fxd=prm.fxd, nABC=nABC, 
+				   simplify = FALSE)
+	sfStop()
+	
+	M <- cbind(M, ss=unlist(ss))
+	M <- M[order(M[,'ss']),]
+	thres.row <- round(nABC*post.prop)
+	Mpost <- M[1:thres.row,]
+	return(list(posteriors=Mpost, priors=priors))
+}
+
+
---
+++ test-fit-ABC.R
@@ -0,0 +1,106 @@
+source("SEmInR_deterministic_fit_ABC.R")
+source("SEmInR_deterministic.R")
+set.seed(1234)
+
+# TRUE PARAMTERS:
+prm.to.fit <- c(
+	latent_mean     = 2,
+	infectious_mean = 2,
+	popSize         = 1E5,
+	R0              = 2.3)
+
+priors <- list(infectious_mean = list('unif', prm=list(1,5)),
+			   latent_mean     = list('unif', prm=list(1,5)),
+			   popSize         = list('lnorm', prm=list(log(1e4),1.1)),
+			   R0              = list('lnorm',prm=list(log(2),0.8))
+)
+priors.prm.to.fit <- priors
+prm.fxd <-  c(
+	horizon = 200,
+	nE = 2,
+	nI = 2,
+	init_I1 = 1,
+	n.time.steps = 500,
+	per.capita = FALSE
+)
+
+
+# Synthetic data
+sim.true <- simul.SEmInR.det(prm.to.fit, prm.fxd)
+df  <- sim.true$ts
+true.inc <- df$inc
+tt <- df$time
+t.obs <- seq(5,20,by=2)
+
+dt  <- tt[2]-tt[1]
+idx <- vector()
+for(k in 1:length(t.obs)) { idx[k] <- which(abs(tt-t.obs[k]) < dt/1.99)[1] }
+t <- tt[idx]
+round.inc <- ceiling(true.inc[idx])
+
+inc.obs <- rpois(n=length(t.obs), lambda = round.inc)
+#inc.obs <- c(4,6,6,8,9,11,12,15) ; plot(inc.obs,log='y')
+# inc.obs <- c(1,3,1,8,9,15,19,23)
+# plot(true.inc)
+
+
+nABC <- 1E4
+post.prop <- 0.01
+nABC*post.prop  
+CI <- 0.95
+
+par(mfrow=c(5,5))
+system.time(
+	fit <- fit.ABC.SEmInR(t.obs, inc.obs, 
+						  prm.fxd, priors.prm.to.fit, 
+						  nABC, post.prop)
+)
+
+Mpost <- fit[['posteriors']]
+
+par(mfrow=c(2,2))
+for(j in 1:(ncol(Mpost)-1)){
+	trueval <- prm.to.fit[[colnames(Mpost)[j]]]
+	hist(Mpost[,j],breaks = min(30,nABC),
+		 col = 'lightgrey',border = NA,
+		 main = colnames(Mpost)[j], 
+		 xlab='',ylab='', yaxt='n',
+		 xlim = range(Mpost[,j],trueval))
+	abline(v=median(Mpost[,j]),lwd=3)
+	abline(v=quantile(Mpost[,j], probs = 0.5+CI/2), lwd=1)
+	abline(v=quantile(Mpost[,j], probs = 0.5-CI/2), lwd=1)
+	abline(v=trueval,lwd=3,lty=2,col='red')
+}
+
+# Sample from posteriors:
+
+Mpost2 <- Mpost[,-ncol(Mpost)]
+
+inc.post <- matrix(ncol=nrow(Mpost2),nrow=length(tt))
+for(i in 1:nrow(Mpost2)){
+	if(i%%10==0) print(i)
+	sim <- simul.SEmInR.det(prm.to.fit=Mpost2[i,], prm.fxd)
+	df  <- sim$ts
+	inc.post[,i] <- df$inc
+	# lines(tt,inc.post[,i],col=rgb(1,0,0,0.2))
+}
+inc.md <- apply(X = inc.post,MARGIN = 1, FUN = quantile, probs= 0.5)
+inc.lo <- apply(X = inc.post,MARGIN = 1, FUN = quantile, probs= 0.5-CI/2)
+inc.hi <- apply(X = inc.post,MARGIN = 1, FUN = quantile, probs= 0.5+CI/2)
+
+
+
+myplot <- function(dolog,showall=FALSE){
+	plot(tt, true.inc,typ='l',lty=2,log=dolog,ylim=1+range(inc.hi,true.inc))
+	points(t.obs, inc.obs,pch=16)
+	lines(tt,inc.md,col=rgb(1,0,0),lwd=2)
+	lines(tt,inc.lo,col=rgb(1,0,0,0.5))
+	lines(tt,inc.hi,col=rgb(1,0,0,0.5))
+	if(showall){
+		for(i in 1:ncol(inc.post)) lines(tt,inc.post[,i],col=rgb(1,0,0,0.1))
+	}
+}
+
+par(mfrow=c(2,1))
+myplot('y', showall=TRUE)
+myplot('')
